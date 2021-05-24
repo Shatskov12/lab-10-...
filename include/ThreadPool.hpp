957d28792b1ad52a672b5ea39dcab833623a68a2
Shatskov12 <qweser2016@mail.ru>
2021-05-25 02:07:30 +0300

    2

--- include/ThreadPool.hpp
+++ include/ThreadPool.hpp
@@ -1,5 +1,5 @@
 // Copyright 2020 Your Name <your_email>
-#ifndef THREAD_POOL_H
+#ifndef INCLUDE_THREADPOOL_HPP_
 #define THREAD_POOL_H
 
 #include <vector>
@@ -11,10 +11,11 @@
 #include <future>
 #include <functional>
 #include <stdexcept>
+#include <utility>
 
 class ThreadPool {
  public:
-  ThreadPool(size_t);
+  explicit ThreadPool(size_t);
   template<class F, class... Args>
   auto enqueue(F&& f, Args&&... args)
   -> std::future<typename std::result_of<F(Args...)>::type>;
@@ -35,19 +36,21 @@ class ThreadPool {
 inline ThreadPool::ThreadPool(size_t threads)
     :   stop(false)
 {
-  for(size_t i = 0;i<threads;++i)
+  for (size_t i = 0; i < threads; ++i)
     workers.emplace_back(
         [this]
         {
-          for(;;)
+          for (;;)
           {
             std::function<void()> task;
 
             {
               std::unique_lock<std::mutex> lock(this->queue_mutex);
               this->condition.wait(lock,
-                                   [this]{ return this->stop || !this->tasks.empty(); });
-              if(this->stop && this->tasks.empty())
+                                   [this]
+                                   { return this->stop ||
+                                            !this->tasks.empty(); });
+              if (this->stop && this->tasks.empty())
                 return;
               task = std::move(this->tasks.front());
               this->tasks.pop();
@@ -55,8 +58,7 @@ inline ThreadPool::ThreadPool(size_t threads)
 
             task();
           }
-        }
-    );
+        });
 }
 
 // add new work item to the pool
@@ -67,15 +69,14 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
   using return_type = typename std::result_of<F(Args...)>::type;
 
   auto task = std::make_shared< std::packaged_task<return_type()> >(
-      std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-  );
+      std::bind(std::forward<F>(f), std::forward<Args>(args)...));
 
   std::future<return_type> res = task->get_future();
   {
     std::unique_lock<std::mutex> lock(queue_mutex);
 
     // don't allow enqueueing after stopping the pool
-    if(stop)
+    if (stop)
       throw std::runtime_error("enqueue on stopped ThreadPool");
 
     tasks.emplace([task](){ (*task)(); });
@@ -92,8 +93,8 @@ inline ThreadPool::~ThreadPool()
     stop = true;
   }
   condition.notify_all();
-  for(std::thread &worker: workers)
+  for (std::thread & worker : workers)
     worker.join();
 }
 
-#endif
+#endif  // INCLUDE_THREADPOOL_HPP_
